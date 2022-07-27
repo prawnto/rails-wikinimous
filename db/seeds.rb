d8740d6a0172185967a7cf75dadea7fe43c5bf42
Jie Lin Lee <lin.leejie@gmail.com>
2022-07-27 22:06:19 +0800

    completed base code for wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title.capitalize,
+    content: Faker::Lorem.paragraph(sentence_count: 6)
+  )
+  article.save!
+end
